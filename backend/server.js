829295be3b841b7046003219068e8da00a06ef6b
riteshrana12-dev <riteshrana677@gmail.com>
2026-03-25 00:36:52 +0530

    feat(server): initialize express app with middleware, routes, and database connection
    - configure express.json and cookie-parser
    - register auth and problem routes
    - establish MongoDB connection before server start

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+// import cors from "cors";
+import authRoutes from "./routes/auth.route.js";
+import problemsRoutes from "./routes/problems.route.js";
+import connectToDb from "./config/db.js";
+
+const app = express();
+app.use(express.json());
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173", // frontend URL
+//     credentials: true,
+//   }),
+// );
+
+app.use(cookieParser());
+
+app.use("/api/v1", authRoutes);
+app.use("/api/v1/problems", problemsRoutes);
+(async () => {
+  try {
+    await connectToDb();
+    app.listen(3000, () => console.log("Server running on port 3000"));
+  } catch (err) {
+    console.error("Failed to connect to DB. Server not started.", err.message);
+    process.exit(1);
+  }
+})();
