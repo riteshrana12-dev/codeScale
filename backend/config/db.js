3ca5e7cc859c484697e77d9cc95d90d262dd0637
riteshrana12-dev <riteshrana677@gmail.com>
2026-03-25 00:53:09 +0530

    feat(db): add MongoDB connection setup using mongoose and environment variables

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config/.env" });
+async function connectToDb() {
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    console.log("DB_URI:", process.env.DB_URI);
+    console.log("DB connected");
+  } catch (err) {
+    console.log("cannot connect to db :", err);
+  }
+}
+
+export default connectToDb;
