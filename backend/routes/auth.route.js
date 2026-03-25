ff3e9fddf7d13e1f0d3daf8ca3bc378f81fe1fd2
riteshrana12-dev <ranaritesh677@gmail.com>
2026-03-25 19:52:37 +0530

    feat(routes): add authentication routes for signup and login
    - Created authRouter using Express Router
    
    - Added POST /signupUser route mapped to registerUser controller
    
    - Added POST /loginUser route mapped to loginUser controller
    
    - Exported authRouter for integration into main server

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import authController from "../controllers/auth.controller.js";
 const authRouter = Router();
 
-authRouter.post("/signupuser", authController.registerUser);
-authRouter.post("/loginuser", authController.loginUser);
+authRouter.post("/signupUser", authController.registerUser);
+authRouter.post("/loginUser", authController.loginUser);
 
 export default authRouter;
