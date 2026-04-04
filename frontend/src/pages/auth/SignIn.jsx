84de63591b62dcbcbcf12617ef81a4718b08a3fe
riteshrana12-dev <ranaritesh677@gmail.com>
2026-04-05 01:53:57 +0530

    feat: import framer-motion and custom useSignIn hook

--- frontend/src/pages/auth/SignIn.jsx
+++ frontend/src/pages/auth/SignIn.jsx
@@ -1,2 +1,4 @@
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import useSignIn from "../../hooks/signIn";
