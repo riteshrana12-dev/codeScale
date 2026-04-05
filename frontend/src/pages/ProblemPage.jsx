766fe5a730c2b8af3aa4f1c9237f5e7a1e09ba4c
riteshrana12-dev <ranaritesh677@gmail.com>
2026-04-06 00:17:37 +0530

    feat: import CodeEditor and ProblemDescription components

--- frontend/src/pages/ProblemPage.jsx
+++ frontend/src/pages/ProblemPage.jsx
@@ -1,3 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from "../api/api.js";
+import CodeEditor from "../components/codeEditor/CodeEditor";
+import ProblemDescription from "../components/codeEditor/ProblemDescription";
