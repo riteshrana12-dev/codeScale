e7636c813009c1ba3ca2b3040fd511fcf6a5f7da
riteshrana12-dev <ranaritesh677@gmail.com>
2026-04-06 00:18:13 +0530

    feat: import ProblemContext hook and framer-motion

--- frontend/src/pages/ProblemPage.jsx
+++ frontend/src/pages/ProblemPage.jsx
@@ -3,3 +3,6 @@ import { useParams } from "react-router-dom";
 import api from "../api/api.js";
 import CodeEditor from "../components/codeEditor/CodeEditor";
 import ProblemDescription from "../components/codeEditor/ProblemDescription";
+
+import { useProblem } from "../context/ProblemContext.jsx";
+import { motion } from "framer-motion";
