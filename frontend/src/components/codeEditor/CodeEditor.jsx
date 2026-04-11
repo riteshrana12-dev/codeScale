4d1276aca0184b187c69f0decf77a98ce9c573bc
riteshrana12-dev <ranaritesh677@gmail.com>
2026-04-12 00:28:59 +0530

    feat: import ProblemContext hook and framer-motion

--- frontend/src/components/codeEditor/CodeEditor.jsx
+++ frontend/src/components/codeEditor/CodeEditor.jsx
@@ -1,6 +1,8 @@
 import { useRef } from "react";
 import Editor from "@monaco-editor/react";
-
 import Language from "./Language";
 import RunCodeButton from "./RunCodeButton";
 import SubmitCodeButton from "./SubmitButton";
+
+import { useProblem } from "../../context/ProblemContext";
+import { motion, AnimatePresence } from "framer-motion";
