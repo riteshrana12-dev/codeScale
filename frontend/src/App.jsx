4df8944b5f0dfbcf0ac1259239d11e4b3b68723c
riteshrana12-dev <ranaritesh677@gmail.com>
2026-04-15 00:57:50 +0530

    refractor: chnaged the file directory of problemList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,21 @@ import { Routes, Route } from "react-router-dom";
 import SignUp from "./pages/auth/SignUp";
 import SignIn from "./pages/auth/SignIn";
 import UserDashboard from "./pages/user/userDashboard";
-import ProblemsList from "./pages/user/ProblemsList";
-import ProblemSelected from "./components/codeEditor/ProblemSelected";
+import ProblemsList from "./pages/ProblemsList";
+// import ProblemSelected from "./components/codeEditor/ProblemSelected";
+import ProblemPage from "./pages/ProblemPage";
 import { ProblemProvider } from "./context/ProblemContext";
+import HomePage from "./pages/HomePage";
 const App = () => {
   return (
     <ProblemProvider>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/signUp" element={<SignUp />} />
         <Route path="/signIn" element={<SignIn />} />
         <Route path="/dashboard" element={<UserDashboard />} />
         <Route path="/problems" element={<ProblemsList />} />
-        <Route path="/problems/:slug" element={<ProblemSelected />} />
+        <Route path="/problems/:slug" element={<ProblemPage />} />
       </Routes>
     </ProblemProvider>
   );
