ea47ab5a18b917ae29be08e48a1d5c88a9290f1e
riteshrana12-dev <ranaritesh677@gmail.com>
2026-04-23 16:11:44 +0530

    refactor(history): introduce detail view by submission ID
    - Added GET /history/detailview/:id route
    - Secured route with JWT_USER auth middleware
    - Connected to getSubmissionViewById controller for detailed problem submission view

--- backend/routes/user_history.route.js
+++ backend/routes/user_history.route.js
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import authMiddleware from "../middleware/auth.middleware.js";
 import getSubmissionHistory from "../controllers/user_history.controller.js";
+import getSubmissionViewById from "../controllers/submissionDetail.controller.js";
 const historyroute = Router();
 
 historyroute.get(
   "/history",
   authMiddleware(process.env.JWT_SECRET_USER),
   getSubmissionHistory,
+);
+
+historyroute.get(
+  "/history/detailview/:id",
+  authMiddleware(process.env.JWT_SECRET_USER),
+  getSubmissionViewById,
 );
 
 export default historyroute;
