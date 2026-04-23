64b051fa2a2e3bc3cef89dd9c11f72baa42b191c
riteshrana12-dev <ranaritesh677@gmail.com>
2026-04-23 16:33:02 +0530

    feat(history): add pagination support to submission history API
    - Implemented page/limit query params with validation
    - Added skip/limit logic for efficient data retrieval
    - Populated problem details (title, difficulty, slug) in history response
    - Included pagination metadata (totalSubmissions, totalPages, currentPage, limit) in API response

--- backend/controllers/user_history.controller.js
+++ backend/controllers/user_history.controller.js
@@ -4,13 +4,29 @@ const getSubmissionHistory = async (req, res) => {
   try {
     const userId = req.user_id;
 
+    let page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+
+    //  Validation: Ensure page/limit aren't negative or zero
+    if (page < 1) page = 1;
+    if (limit < 1) limit = 10;
+
+    const skip = (page - 1) * limit;
     // 1. Find submissions for this user
     // 2. Sort by 'createdAt' so the newest ones are at the top
     // 3. .populate('problemId', 'title difficulty') joins the Problem data
+    // 4. .skip to skip some number of data
+    // 5. .limit is to limit the data to get of fixed size
+    const totalSubmissions = await submissionModel.countDocuments({ userId });
+
     const history = await submissionModel
-      .find({ userId })
+      .find({ userId }, "-code -testCaseResults")
       .sort({ createdAt: -1 })
-      .populate("problemId", "title difficulty");
+      .limit(limit)
+      .skip(skip)
+      .populate("problemId", "title difficulty slug");
+
+    const totalPages = Math.ceil(totalSubmissions / limit);
 
     if (!history || history.length === 0) {
       return res.status(200).json({
@@ -25,6 +41,12 @@ const getSubmissionHistory = async (req, res) => {
       message: "submission history",
       count: history.length,
       data: history,
+      pagination: {
+        totalSubmissions,
+        totalPages,
+        currentPage: page,
+        limit,
+      },
     });
   } catch (err) {
     res.status(500).json({
